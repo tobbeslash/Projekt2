8ee79a3843707e27bee2f03be7134948f1063a19
tobbeslash <svenssontobias96@gmail.com>
2015-05-24 23:27:11 +0200

    haha

--- server/php/getPokemonTypes.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin:*");
-include_once("server.php");
-$serverObj = new Server();
-$data = $serverObj->GetPokemonTypes();
-$serverObj = null;
-echo json_encode($data);
-?>
--- server/php/getjson.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin:*");
-include("server.php");
-$serverObj = new Server();
-$data = $serverObj->GetPokemon();
-$serverObj = null;
-echo json_encode($data);
-?>
